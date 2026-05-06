cb6afc609441c8cc4aac99f150f05d816e7fab07
Pathy - Pedro <pathysystems@gmail.com>
2026-05-06 14:05:13 -0300

    correrçãp

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,3 @@
-// Mapeia as variáveis do env.php para o objeto CONFIG esperado pelos scripts
 const CONFIG = {
   SUPABASE_URL: window.__ENV__?.SUPABASE_URL || '',
   SUPABASE_KEY: window.__ENV__?.SUPABASE_KEY || ''
